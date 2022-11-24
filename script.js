aeae42373fdf615ca028b09a6e7cb345c41724da
Ines-Dhehibi <inesgadria.dhehibi@gmail.com>
2022-11-24 15:18:00 +0100

    projet qu'on a gagné les femmes

---
+++ script.js
@@ -0,0 +1,8 @@
+alert("J'apprends Git")
+function sayhello(){
+    document.write("MERCI POUR VOTRE INSCRIPTION")
+}
+
+function contact(){
+    document.write("NOUS AVONS BIEN RECU VOTRE FORMULAIRE")
+}
